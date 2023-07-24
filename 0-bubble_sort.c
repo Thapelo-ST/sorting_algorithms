b80a5ea41d0cf59d42fc5ba0fa0a5422b979bc49
Thapelo-ST <tsamkelo96@gmail.com>
2023-07-24 03:23:01 +0200

    fix 2

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+#include <stdbool.h>
 
 /**
  * bubble_sort - sorts the array of intergers in accending order using bubble
@@ -12,6 +13,7 @@ void bubble_sort(int *array, size_t size)
 	/* declare variables*/
 	int temp;
 	size_t i, j;
+	bool swapped = false;
 
 	/* check if size and array are not empty*/
 	if (array == 0 && size == 0)
@@ -20,6 +22,7 @@ void bubble_sort(int *array, size_t size)
 	/* loop to enter the list/array */
 	for (i = 0; i < size - 1; i++)
 	{
+		swapped = false;
 		/*loop to compare*/
 		for (j = 0; j < size - i - 1; j++)
 		{
@@ -29,9 +32,13 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+				swapped = true;
 			}
 		}
 		/*print each time the outer loop completes */
 		print_array(array, size);
+
+		if (!swapped)
+			break;
 	}
 }
